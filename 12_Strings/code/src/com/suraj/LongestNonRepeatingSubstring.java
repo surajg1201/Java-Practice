f5a2308e6bec39f3dfb3393c2c53a92187da933b
surajg1201 <surajg1201@gmail.com>
2026-01-19 11:30:02 +0530

    added

---
+++ 12_Strings/code/src/com/suraj/LongestNonRepeatingSubstring.java
@@ -0,0 +1,28 @@
+package com.suraj;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestNonRepeatingSubstring {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(longestSubstring(s));
+    }
+
+    static int longestSubstring(String s){
+        int l = 0;
+        int res = 0;
+        Set<Character> seen= new HashSet<>();
+        int r=0;
+        while(r<s.length()){
+            while(seen.contains(s.charAt(r))) {
+                seen.remove(s.charAt(l));
+                l++;
+            }
+            seen.add(s.charAt(r));
+            res = Math.max(res,r-l+1);
+            r++;
+        }
+        return res;
+    }
+}
