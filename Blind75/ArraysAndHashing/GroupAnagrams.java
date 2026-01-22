d9543f941b1712e221855a0d0cabe40fcc47729f
surajg1201 <surajg1201@gmail.com>
2026-01-22 11:59:50 +0530

    updated

--- Blind75/ArraysAndHashing/GroupAnagrams.java
+++ Blind75/ArraysAndHashing/GroupAnagrams.java
@@ -8,13 +8,16 @@ public class GroupAnagrams {
         System.out.println(groupAnagrams(strs));
     }
     static List<List<String>> groupAnagrams(String[] strs) {
+
         Map<String, List<String>> res = new HashMap<>();
         for (String s : strs) {
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedS = new String(charArray);
-            res.putIfAbsent(sortedS, new ArrayList<>());
-            res.get(sortedS).add(s);
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
         }
         return new ArrayList<>(res.values());
     }
